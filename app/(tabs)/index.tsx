a87e029f554205b604e55e44eec02e01217e6f55
avinash <avinashnanda008@gmail.com>
2024-02-21 18:36:44 +0530

    added: order item card

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,13 @@
-import OrderCard from "@/components/Card";
-import CardComponent from "@/components/Card";
 import CreateOrderButton from "@/components/CreateOrderButton";
-import HeadingChips from "@/components/HeadingChips";
+import OrderList from "@/components/OrderList";
 import { ScrollView, View } from "react-native";
-import { Divider, Text } from "react-native-paper";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
   return (
     <View style={{ flex: 1, paddingVertical: 5 }}>
       <CreateOrderButton />
       <ScrollView>
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
+        <OrderList heading="Today's Orders" />
       </ScrollView>
     </View>
   );
---
+++ components/ListItem.tsx
@@ -0,0 +1,37 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Pressable, View, useColorScheme } from "react-native";
+import { List, MD3DarkTheme, MD3LightTheme } from "react-native-paper";
+
+const ListItem = ({ name, icon }: { name: string; icon: string }) => {
+  const theme = useColorScheme();
+  return (
+    <Pressable
+      android_ripple={{
+        color:
+          theme === "dark"
+            ? MD3DarkTheme.colors.onSecondary
+            : MD3LightTheme.colors.onSecondary,
+      }}
+      unstable_pressDelay={200}
+    >
+      <List.Item
+        titleStyle={{ fontSize: 18 }}
+        title={name}
+        descriptionNumberOfLines={2}
+        description={
+          "Join us for an enchanting evening under the stars, celebrating love and unity with music, laughter, and cherished memories."
+        }
+        left={({ color, style }) => (
+          <MaterialCommunityIcons
+            name="truck"
+            color={color}
+            size={25}
+            style={style}
+          />
+        )}
+      />
+    </Pressable>
+  );
+};
+
+export default ListItem;
