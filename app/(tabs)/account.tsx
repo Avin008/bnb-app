793a1edb481e5a8983901d6647ec27e55756d23f
avinash <avinashnanda008@gmail.com>
2024-02-24 00:07:09 +0530

    added: profile page component

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -1,8 +1,40 @@
-import { View, Text } from "react-native";
+import { View, Image } from "react-native";
+import { Text, Card } from "react-native-paper";
+
 const AccountScreen = () => {
   return (
     <View>
-      <Text>Search Page</Text>
+      <View
+        style={{ display: "flex", alignItems: "center", padding: 20, gap: 5 }}
+      >
+        <Image
+          source={{
+            uri: "https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg",
+          }}
+          borderRadius={50}
+          width={80}
+          height={80}
+        />
+        <Text variant="headlineMedium">John Doe</Text>
+        <Text variant="titleSmall">You have the premium plan $200 / month</Text>
+      </View>
+      <View style={{ padding: 10, display: "flex", gap: 10 }}>
+        <Card>
+          <Card.Content>
+            <Text variant="titleMedium">Change your plan</Text>
+          </Card.Content>
+        </Card>
+        <Card>
+          <Card.Content>
+            <Text variant="titleMedium">Company details</Text>
+          </Card.Content>
+        </Card>
+        <Card>
+          <Card.Content>
+            <Text variant="titleMedium">Logout</Text>
+          </Card.Content>
+        </Card>
+      </View>
     </View>
   );
 };
