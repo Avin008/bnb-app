1e1746490d32fd758adaff46c20224b521f376aa
Avinash <avinashnanda008@gmail.com>
2024-03-08 09:51:22 +0530

    added: pager view second page

--- components/FilterByDate.tsx
+++ components/FilterByDate.tsx
@@ -1,5 +1,5 @@
 import Colors from "@/constants/Colors";
-import { View, useColorScheme } from "react-native";
+import { Pressable, View, useColorScheme } from "react-native";
 import PagerView from "react-native-pager-view";
 import { Text, useTheme } from "react-native-paper";
 
@@ -27,7 +27,9 @@ const FilterByDate = () => {
             { day: "FRI", dayNum: 6 },
             { day: "SAT", dayNum: 7 },
           ].map((x, i) => (
-            <View
+            <Pressable
+              android_ripple={{ color: "#8ac5ef" }}
+              unstable_pressDelay={200}
               key={i}
               style={{
                 display: "flex",
@@ -36,13 +38,50 @@ const FilterByDate = () => {
                 alignItems: "center",
                 justifyContent: "center",
                 borderWidth: 0.4,
-                backgroundColor:
-                  theme === "light" ? Colors.dark.tint : Colors.light.tint,
+                backgroundColor: theme === "light" ? Colors.dark.tint : "black",
               }}
             >
               <Text>{x.day}</Text>
               <Text variant="bodyLarge">{x.dayNum}</Text>
-            </View>
+            </Pressable>
+          ))}
+        </View>
+        <View
+          key="2"
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "stretch",
+            justifyContent: "space-around",
+            padding: 10,
+          }}
+        >
+          {[
+            { day: "SUN", dayNum: 8 },
+            { day: "MON", dayNum: 9 },
+            { day: "TUE", dayNum: 10 },
+            { day: "WED", dayNum: 11 },
+            { day: "THU", dayNum: 12 },
+            { day: "FRI", dayNum: 13 },
+            { day: "SAT", dayNum: 14 },
+          ].map((x, i) => (
+            <Pressable
+              android_ripple={{ color: "#8ac5ef" }}
+              unstable_pressDelay={200}
+              key={i}
+              style={{
+                display: "flex",
+                borderRadius: 5,
+                width: 50,
+                alignItems: "center",
+                justifyContent: "center",
+                borderWidth: 0.4,
+                backgroundColor: theme === "light" ? Colors.dark.tint : "black",
+              }}
+            >
+              <Text>{x.day}</Text>
+              <Text variant="bodyLarge">{x.dayNum}</Text>
+            </Pressable>
           ))}
         </View>
       </PagerView>
